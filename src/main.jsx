b93660f659f78f6cf83366c673fd0b5cb1b860a8
whatever9326 <whatever9326@gmail.com>
2025-06-13 10:39:30 -0600

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import GeneradorPedidos from './GeneradorPedidos'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <GeneradorPedidos />
-  </React.StrictMode>
-)
